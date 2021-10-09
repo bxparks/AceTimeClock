c0281688adfc70d80ec072c80710004ad67c2dd4
Brian Park <brian@xparks.net>
2021-10-09 12:55:19 -0700

    AutoBenchmark: Add benchmark for SystemClockLoop::loop()

--- examples/AutoBenchmark/Benchmark.cpp
+++ examples/AutoBenchmark/Benchmark.cpp
@@ -4,13 +4,16 @@
 
 #include <stdint.h>
 #include <Arduino.h>
+#include <AceCommon.h> // printUint32AsFloat3To()
 #include <AceTimeClock.h>
+#include <ace_time/testing/FakeClock.h>
 #include "Benchmark.h"
 
-using namespace ace_time;
+using ace_time::clock::SystemClockLoop;
+using ace_time::testing::FakeClock;
 
 #if defined(ARDUINO_ARCH_AVR)
-const uint32_t COUNT = 1000;
+const uint32_t COUNT = 5000;
 #elif defined(ARDUINO_ARCH_SAMD)
 const uint32_t COUNT = 5000;
 #elif defined(ARDUINO_ARCH_STM32)
@@ -23,13 +26,11 @@ const uint32_t COUNT = 20000;
 const uint32_t COUNT = 20000;
 #elif defined(EPOXY_DUINO)
 // Linux or MacOS
-const uint32_t COUNT = 50000;
+const uint32_t COUNT = 100000;
 #else
 // A generic Arduino board that we have not looked at.
 const uint32_t COUNT = 10000;
 #endif
-
-const uint32_t MILLIS_TO_NANO_PER_ITERATION = ((uint32_t) 1000000 / COUNT);
 
 // The compiler is extremelly good about removing code that does nothing. This
 // volatile variable is used to create side-effects that prevent the compiler
@@ -45,43 +46,51 @@ void disableOptimization(uint32_t value) {
   guard ^= (value >> 24) & 0xff;
 }
 
-// Type declaration of the Lambda.
-typedef void (*Lambda)();
-
-// A small helper that runs the given lamba expression in a loop
-// and returns how long it took.
-unsigned long runLambda(Lambda lambda) {
-  yield();
-  uint32_t count = COUNT;
-  unsigned long startMillis = millis();
-  while (count--) {
-    lambda();
-  }
-  unsigned long elapsedMillis = millis() - startMillis;
-  yield();
-  return elapsedMillis;
+// Print nanos as micros with 3 decimal places.
+static void printMicrosPerIteration(uint32_t nanos) {
+  ace_common::printUint32AsFloat3To(SERIAL_PORT_MONITOR, nanos);
 }
 
-// Given total elapsed time in millis, print micros per iteration as a floating
-// point number (without using floating point operations).
-//
-// Sometimes, the elapsedMillis is negative. This happens on some benchmarks on
-// higher powered CPUs where the thing being measured is so quickly executed
-// that the empty loop overhead can take a longer. Print "-0.000" if that
-// occurs.
-static void printMicrosPerIteration(long elapsedMillis) {
-  if (elapsedMillis < 0) {
-    SERIAL_PORT_MONITOR.print(F("  -0.000"));
-    return;
+//-----------------------------------------------------------------------------
+
+FakeClock fakeClock;
+
+SystemClockLoop systemClockLoop(&fakeClock, nullptr);
+
+/**
+ * Call SystemClockLoop::loop() COUNT number of times and return the total
+ * number of micros elapsed.
+ */
+uint32_t runSystemClockLoop() {
+  fakeClock.isResponseReady(true);
+
+  yield();
+  uint32_t count = COUNT;
+  uint16_t internal = 0;
+  uint32_t startMicros = micros();
+  while (count--) {
+    // Simulate the FakeClock incrementing by 1 second every 1000 iterations.
+    // It's much faster than real time, but should be good enough to get a rough
+    // estimate of how much long SystemClockLoop::loop() takes.
+    if (internal == 1000) {
+      uint32_t now = fakeClock.getNow();
+      disableOptimization(now);
+      fakeClock.setNow(now + 1);
+      internal = 0;
+    }
+    internal++;
+
+    systemClockLoop.loop();
   }
-  unsigned long nanos = elapsedMillis * MILLIS_TO_NANO_PER_ITERATION;
-  uint16_t whole = nanos / 1000;
-  uint16_t frac = nanos % 1000;
-  SERIAL_PORT_MONITOR.print(' ');
-  SERIAL_PORT_MONITOR.print(whole);
-  SERIAL_PORT_MONITOR.print('.');
-  ace_common::printPad3To(SERIAL_PORT_MONITOR, frac, '0');
+  uint32_t elapsedMicros = micros() - startMicros;
+  yield();
+
+  return elapsedMicros;
 }
 
 void runBenchmarks() {
+  uint32_t elapsedNanos = runSystemClockLoop() * 1000 / COUNT;
+  SERIAL_PORT_MONITOR.print(F("SystemClockLoop::loop() "));
+  printMicrosPerIteration(elapsedNanos);
+  SERIAL_PORT_MONITOR.println();
 }
