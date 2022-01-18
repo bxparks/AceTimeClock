99384ca845a833e6fa797f5d5b4aafe081153da6
Brian Park <brian@xparks.net>
2022-01-18 13:24:23 -0800

    docs: Regenerate doxygen for v1.0.4

--- docs/html/search/all_9.js
+++ docs/html/search/all_9.js
@@ -1,5 +1,14 @@
 var searchData=
 [
-  ['loop_34',['loop',['../classace__time_1_1clock_1_1SystemClockLoopTemplate.html#a2e67ec87c3546390e35d280aefb21d49',1,'ace_time::clock::SystemClockLoopTemplate']]],
-  ['lsb_35',['lsb',['../structace__time_1_1hw_1_1HardwareTemperature.html#a53bd07c5224a1ed74658e78c4fef0aef',1,'ace_time::hw::HardwareTemperature']]]
+  ['kdefaultntpserver_26',['kDefaultNtpServer',['../classace__time_1_1clock_1_1EspSntpClock.html#aafacb27a12ce1bfc499c39c295871644',1,'ace_time::clock::EspSntpClock']]],
+  ['kdefaulttimeoutmillis_27',['kDefaultTimeoutMillis',['../classace__time_1_1clock_1_1EspSntpClock.html#a31f80a7e10ffc094cefec417a205300e',1,'ace_time::clock::EspSntpClock']]],
+  ['keepalive_28',['keepAlive',['../classace__time_1_1clock_1_1SystemClockTemplate.html#a73dc75820d6413a3baae46f2b292f5a1',1,'ace_time::clock::SystemClockTemplate']]],
+  ['kinvalidseconds_29',['kInvalidSeconds',['../classace__time_1_1clock_1_1Clock.html#ae4fb852c09fbb9e37cc5764787dbf094',1,'ace_time::clock::Clock']]],
+  ['klocalport_30',['kLocalPort',['../classace__time_1_1clock_1_1NtpClock.html#a23ba1065b516b1e37b154b86d36357ad',1,'ace_time::clock::NtpClock']]],
+  ['kntpservername_31',['kNtpServerName',['../classace__time_1_1clock_1_1NtpClock.html#a282fa5d05aba3d8d7959ecf2602cc70b',1,'ace_time::clock::NtpClock']]],
+  ['krequesttimeout_32',['kRequestTimeout',['../classace__time_1_1clock_1_1NtpClock.html#ae8ee51f3f5394f7da1519e3f70a3af24',1,'ace_time::clock::NtpClock']]],
+  ['ksyncstatuserror_33',['kSyncStatusError',['../classace__time_1_1clock_1_1SystemClockTemplate.html#a28242e7bcbe16ffb42275208e759ac91',1,'ace_time::clock::SystemClockTemplate']]],
+  ['ksyncstatusok_34',['kSyncStatusOk',['../classace__time_1_1clock_1_1SystemClockTemplate.html#af104c93bdf50ec94fda5bd9e3b018870',1,'ace_time::clock::SystemClockTemplate']]],
+  ['ksyncstatustimedout_35',['kSyncStatusTimedOut',['../classace__time_1_1clock_1_1SystemClockTemplate.html#abd62cfa85048d37e97100d6c8287748f',1,'ace_time::clock::SystemClockTemplate']]],
+  ['ksyncstatusunknown_36',['kSyncStatusUnknown',['../classace__time_1_1clock_1_1SystemClockTemplate.html#ac7d915d7c09a6452ac7cb0ac2616bea8',1,'ace_time::clock::SystemClockTemplate']]]
 ];
--- docs/html/search/all_a.js
+++ docs/html/search/all_a.js
@@ -1,7 +1,5 @@
 var searchData=
 [
-  ['millis_36',['millis',['../classace__time_1_1hw_1_1ClockInterface.html#ace0e9997bab79c73031864ac19923e78',1,'ace_time::hw::ClockInterface']]],
-  ['minute_37',['minute',['../structace__time_1_1hw_1_1HardwareDateTime.html#a31d39de29ab0990bea28c228b07d7eb2',1,'ace_time::hw::HardwareDateTime']]],
-  ['month_38',['month',['../structace__time_1_1hw_1_1HardwareDateTime.html#ac008217ed89d235c069da2210fdbb44e',1,'ace_time::hw::HardwareDateTime']]],
-  ['msb_39',['msb',['../structace__time_1_1hw_1_1HardwareTemperature.html#a307064c14c34483ff682e462b39d13c0',1,'ace_time::hw::HardwareTemperature']]]
+  ['loop_37',['loop',['../classace__time_1_1clock_1_1SystemClockLoopTemplate.html#a2e67ec87c3546390e35d280aefb21d49',1,'ace_time::clock::SystemClockLoopTemplate']]],
+  ['lsb_38',['lsb',['../structace__time_1_1hw_1_1HardwareTemperature.html#a53bd07c5224a1ed74658e78c4fef0aef',1,'ace_time::hw::HardwareTemperature']]]
 ];
